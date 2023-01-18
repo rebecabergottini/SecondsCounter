ab0b8155bfff06bba0fa4c2cfa2529d0ed1542b8
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-01-18 15:44:25 +0000

    my commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<div className="container row bg-dark align-items-center justify-content-center mx-auto" style={{width: "1000px", height:"150px"}}>
+		<div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{<i className="fa fa-clock">
+				</i>}</h1>
+        </div>
+      </div>
+	<div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{props.numero6}</h1>
+        </div>
+      </div>	
+	<div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{props.numero5}</h1>
+        </div>
+      </div>	
+	<div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{props.numero4}</h1>
+        </div>
+      </div>	
+	<div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{props.numero3}</h1>
+        </div>
+      </div>
+	<div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{props.numero2}</h1>
+        </div>
+      </div>
+	  <div className="card text-white mx-2 bg-dark border border-light border-opacity-25" style={{width: "5rem", height:"7rem"}}>
+        <div className="card-body position-absolute top-50 start-50 translate-middle">
+          <h1 className="card-title">{props.numero1}</h1>
+        </div>
+      </div>
+	  </div>				
+		</>
 	);
 };
 
